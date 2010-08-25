ff82c540322b25d47b96994ccba9559a57ab1ef1
Ryan Fox <foxryan@gmail.com>
2010-08-25 02:32:40 -0400

    Important comment about clone() in World::Feature

--- World/Feature.pm
+++ World/Feature.pm
@@ -11,6 +11,7 @@ has 'on_touch' => ( isa => 'CodeRef', is => 'ro', default => sub{sub{1}} );
 has 'x' => (isa => 'Int', is => 'rw');
 has 'y' => (isa => 'Int', is => 'rw');
 
+# Need to override this if a child class has more complex data.
 sub clone
 {
 	my $self = shift;
