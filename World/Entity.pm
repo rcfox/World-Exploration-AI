c128ec1e822e76eea033cd6a948ad8c01d8fdbd6
Ryan Fox <foxryan@gmail.com>
2010-08-29 01:50:25 -0400

    Optimized the learn_map() function in World::Entity to make it only look at the part of the map that can be seen. Also, you can now call update() on an Entity to draw the parts of the map that the entity has seen before.

--- World/Entity.pm
+++ World/Entity.pm
@@ -5,7 +5,7 @@ with 'Positionable', 'Drawable';
 
 has 'facing' => (isa => 'Num', is => 'rw', default => 0);
 
-has 'sight_range' => (isa => 'Int', is => 'rw', default => 10);
+has 'sight_range' => (isa => 'Int', is => 'rw', default => 5);
 
 has 'name' => (isa => 'Str', is => 'rw');
 
@@ -20,12 +20,13 @@ has 'map_memory' =>
 	    {
 		    my $self = shift;
 		    my @map;
-		    my $nothing = World::Feature->new(char=>' ');
+		    my $nothing = World::Feature->new(char=>' ',surface=>$self->surface,gfx_color=>0);
 		    for(my $y = 0; $y < $self->room->height; ++$y)
 		    {
 			    my @row;
 			    for (my $x = 0; $x < $self->room->width; ++$x)
 			    {
+				    $nothing->place($x,$y);
 				    push @row, $nothing;
 			    }
 			    push @map, \@row;
@@ -55,16 +56,18 @@ sub learn_map
 	my $self = shift;
 	my @map = @{$self->fov};
 
-	for(my $y = 0; $y < $self->room->height; ++$y)
+	for(my $y = $self->y-($self->sight_range-1); $y < $self->y+($self->sight_range-1); ++$y)
 	{
-		for(my $x = 0; $x < $self->room->width; ++$x)
+		for(my $x = $self->x-($self->sight_range-1); $x < $self->x+($self->sight_range-1); ++$x)
 		{
-			my $tx = $x-$self->x+$self->sight_range;
-			my $ty = $y-$self->y+$self->sight_range;
-			if ($tx > 0 && $ty > 0 && $map[$tx][$ty])
+			my $ty = $y - ($self->y-$self->sight_range);
+			my $tx = $x - ($self->x-$self->sight_range);
+
+			if ($x >= 0 && $y >= 0 && $map[$tx][$ty])
 			{
 				my $memory = $self->map_memory->[$y]->[$x];
 				my $actual = $self->room->map->[$y]->[$x];
+
 				if (!$memory->compare($actual))
 				{
 					$self->map_memory->[$y]->[$x] = $actual->clone;
@@ -169,6 +172,29 @@ sub cast_ray
 	}
 
 	return $map;
+}
+
+sub update
+{
+	my $self = shift;
+	for (my $y = 0; $y < $self->room->height; ++$y)
+	{
+		for(my $x = 0; $x < $self->room->width; ++$x)
+		{
+			$self->map_memory->[$y]->[$x]->draw();
+		}
+	}
+
+	my @entities = @{$self->room->entities};
+	my $nothing = World::Feature->new(char=>' ',surface=>$self->surface,gfx_color=>0);
+	foreach (@entities)
+	{
+		my ($x,$y) = ($_->x,$_->y);
+		if (!$self->map_memory->[$y]->[$x]->compare($nothing))
+		{
+			$_->draw();
+		}
+	}
 }
 
 1;
